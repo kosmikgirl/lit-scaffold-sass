2bba3b2db61fc200e180517e58e691f856e6f11e
Evert van der Horst <evert.van.der.horst@mediamonks.com>
2021-11-29 10:44:36 +0100

    refactor - add route param enum to page that uses route params

--- src/page/about-page.ts
+++ src/page/about-page.ts
@@ -1,13 +1,15 @@
 import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
-import {RouteParamType} from '../router/routes';
+import {RouteParamEnum, RouteParamType} from '../router/routes';
 
 @customElement('about-page')
 export default class AboutPage extends LitElement {
   @property({type: Object})
-  routeParams: RouteParamType = {id: 'ID'};
+  routeParams: RouteParamType = {[RouteParamEnum.id]: 'ID'};
 
   render() {
-    return html`<div>AboutPage ${this.routeParams?.id || 'no id'}</div>`;
+    return html`<div>
+      AboutPage ${this.routeParams?.[RouteParamEnum.id] || 'no id'}
+    </div>`;
   }
 }
