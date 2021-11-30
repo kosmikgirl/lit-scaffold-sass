0deefbf03aaad6c9f7d4ef2c66a2d9a14070a980
Evert van der Horst <evert.van.der.horst@mediamonks.com>
2021-11-30 17:07:48 +0100

    refactor - update scripts to generate locale text

--- src/data/locale/es-419.ts
+++ src/data/locale/es-419.ts
@@ -2,12 +2,13 @@
     // Do not modify this file by hand!
     // Re-generate this file by running lit-localize
 
-
-
+    
+    
 
     /* eslint-disable no-irregular-whitespace */
     /* eslint-disable @typescript-eslint/no-explicit-any */
 
     export const templates = {
-      's3ca5029ac693b7d5': `HomePage es-419`,
+      's3ca5029ac693b7d5': `página principal`,
     };
+  
