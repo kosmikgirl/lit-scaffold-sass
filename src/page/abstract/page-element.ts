e55ad6cb15c55a3dd181a5fc084a605cf6548e0d
Lucas Johnson <lucas.johnson@mediamonks.com>
2021-12-02 11:48:50 +0100

    remaned page metadate file type, and moved types to barrel file

--- src/page/abstract/page-element.ts
+++ src/page/abstract/page-element.ts
@@ -1,13 +1,16 @@
 import {LitElement} from 'lit';
 import {property} from 'lit/decorators.js';
-import {RouteData} from '../../data/type/route-types';
-import {PageMetadata} from '../../data/type/seo';
+import {RouteData, PageMetadata} from '../../data/type/index';
 import {setPageTitle} from '../../store/module/app';
 import store from '../../store/store';
 
 export default abstract class PageElement extends LitElement {
   private _pageMetadata: PageMetadata = {
     title: '',
+    description: '',
+    banner: '',
+    bannerAlt: '',
+    contentType: '',
   };
 
   constructor(pageMetadata: PageMetadata) {
--- src/store/module/app.ts
+++ src/store/module/app.ts
@@ -1,6 +1,6 @@
 import {createSlice} from '@reduxjs/toolkit';
 import {PayloadAction} from '@reduxjs/toolkit/dist/createAction';
-import {PageMetadata} from '../../data/type/seo';
+import {PageMetadata} from '../../data/type/index';
 
 export type AppState = {
   isInitialized: boolean;
