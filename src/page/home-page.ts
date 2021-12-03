14ab133ed5be9d0afe21e0d74c915d443cf34960
Martín Termini <martin.termini@mediamonks.com>
2021-12-03 11:07:02 +0100

    minor rename refactor and import route fix

--- src/page/home-page.ts
+++ src/page/home-page.ts
@@ -1,12 +1,12 @@
 import {html} from 'lit';
 import {customElement} from 'lit/decorators.js';
-import {msg, localized} from '@lit/localize/lit-localize';
+import {msg as localize, localized} from '@lit/localize/lit-localize';
 import PageElement from './abstract/page-element';
 
 @localized()
 @customElement('home-page')
 export default class HomePage extends PageElement {
   render() {
-    return html`<div>${msg('HomePage')}</div>`;
+    return html`<div>${localize('HomePage')}</div>`;
   }
 }
--- src/router/router.ts
+++ src/router/router.ts
@@ -5,7 +5,7 @@ import {
   isLocalizationEnabled,
 } from '../config/locale-config';
 import {RouteNames} from '../data/enum/route-enums';
-import {allLocales} from '../data/locale-codes';
+import {allLocales} from '../data/i18n/locale-codes';
 
 const localizePath = (path: string, locale: string) => {
   path = path.startsWith('/') ? path : `/${path}`;
