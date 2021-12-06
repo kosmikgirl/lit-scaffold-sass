19edcca621d509f6196dd1028ebd796417563a18
Lucas Johnson <lucas.johnson@mediamonks.com>
2021-12-06 18:37:59 +0100

    moved favicons to static folder

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import html from '@web/rollup-plugin-html';
 import {copy} from '@web/rollup-plugin-copy';
 import resolve from '@rollup/plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser';
-import minifyHTML from 'rollup-plugin-minify-html-literals';
 import summary from 'rollup-plugin-summary';
 import typescript from 'rollup-plugin-typescript2';
 import replace from '@rollup/plugin-replace';
@@ -30,7 +29,6 @@ export default commandLineArgs => {
       }),
       typescript(),
       resolve(),
-      minifyHTML(),
       terser({
         ecma: 2020,
         module: true,
@@ -46,8 +44,7 @@ export default commandLineArgs => {
         },
       }),
       copy({
-        patterns: ['robots.txt'],
-        rootDir: './src',
+        patterns: ['./static/**/*', './src/robots.txt'],
       }),
       summary(),
     ],
