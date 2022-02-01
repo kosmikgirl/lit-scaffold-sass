8ec307c909de4b8d19530c936c9d5034e3778228
Evert van der Horst <evert.van.der.horst@mediamonks.com>
2022-02-01 17:20:53 +0100

    refactor - rename main to root-element and separate styling from that class

---
+++ src/root-element/styles.ts
@@ -0,0 +1,12 @@
+import {css} from 'lit';
+
+export const styles = css`
+  :host {
+    display: block;
+    padding: 20px;
+    max-width: 60%;
+    margin: 0 auto;
+    background: var(--var-background-primary);
+    color: var(--var-foreground-primary);
+  }
+`;
